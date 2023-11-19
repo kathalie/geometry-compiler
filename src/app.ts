4f7e005cf9ea4fc2ccffdbc8ab5f8e48d58b14fd
Kathryn Verkhohliad <kathrine312003@gmail.com>
2023-11-19 01:32:34 +0100

    fixed cors policy

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import JXG from 'jsxgraph';
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send([
